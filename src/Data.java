fc9abf43bc54fc645522a70d70599ced0ffe9ca8
lygkas-pixel <lygkas.kek@gmail.com>
2026-05-25 14:47:13 +0300

    change position of username search

--- src/Data.java
+++ src/Data.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.Scanner;
 import  java.util.ArrayList;
 
@@ -5,35 +6,46 @@ import  java.util.ArrayList;
 public class Data {
     static ArrayList<User> datas = new ArrayList<>();
 
-     public void usersdata()
 
-     {
-
+    public void usersdata() {
 
 
-             User d1 = new User();
-             System.out.println("parakalw vale username");
-             Scanner username = new Scanner(System.in);
-             d1.username = username.nextLine();
-         for (User u :Data.datas) {
-             if (u.username.equals(d1.username)) {
-                 System.out.println("username arleady exist");
-                 return;                                                                                                // elenxi an yparxei to username ston pinaka kai yparxei se vgazei sthn arxi toy menu
-             }
-         }
-             datas.add(d1);
-             System.out.println("parakalw vale password");
-             Scanner password = new Scanner(System.in);
-             d1.password = password.nextLine();
-             System.out.println("parakalw valte email");
-             Scanner emails = new Scanner(System.in);
-             d1.email = emails.nextLine();
+        User d1 = new User();
+        System.out.println("parakalw vale username");
+        Scanner username = new Scanner(System.in);
+        d1.username = username.nextLine();
 
 
+        datas.add(d1);
+        System.out.println("parakalw vale password");
+        Scanner password = new Scanner(System.in);
+        d1.password = password.nextLine();
+        System.out.println("parakalw valte email");
+        Scanner emails = new Scanner(System.in);
+        d1.email = emails.nextLine();
+        Data data = new Data();
+        boolean exist = data.searchuser(d1.username);                                    // poy mpainei auto????
+        if (exist) {
+            System.out.println("username arleady exist");
+            Data repait = new Data();
+            repait.usersdata();                                                                                             //epistrefi apo thn arxh
+        } else {
 
+        }
 
-             System.out.println(d1.username + " " + d1.password + " " + d1.email);
-         }
-     }
+        System.out.println(d1.username + " " + d1.password + " " + d1.email);
+    }
 
+    public boolean searchuser(String username) {
+
+        for (User u : Data.datas) {
+            if (u.username.equals(username)) {
+                System.out.println("username arleady exist");
+                return true;
+                                                                                                                         // elenxi an yparxei to username ston pinaka kai yparxei se vgazei sthn arxi toy menu
+            }
+        }
+        return false;
+    }
+}
 
