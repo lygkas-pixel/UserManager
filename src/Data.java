bc0e8f56bd0f255e53a8ba00fa29c68c6c2ae4d9
lygkas-pixel <lygkas.kek@gmail.com>
2026-06-02 00:14:08 +0300

    username check change position

--- src/Data.java
+++ src/Data.java
@@ -14,15 +14,6 @@ public class Data {
         System.out.println("parakalw vale username");
         Scanner username = new Scanner(System.in);
         d1.username = username.nextLine();
-
-
-        datas.add(d1);
-        System.out.println("parakalw vale password");
-        Scanner password = new Scanner(System.in);
-        d1.password = password.nextLine();
-        System.out.println("parakalw valte email");
-        Scanner emails = new Scanner(System.in);
-        d1.email = emails.nextLine();
         Data data = new Data();
         boolean exist = data.searchuser(d1.username);                                    // poy mpainei auto????
         if (exist) {
@@ -32,20 +23,27 @@ public class Data {
         } else {
 
         }
+        datas.add(d1);
+        System.out.println("parakalw vale password");
+        Scanner password = new Scanner(System.in);
+        d1.password = password.nextLine();
+        System.out.println("parakalw valte email");
+        Scanner emails = new Scanner(System.in);
+        d1.email = emails.nextLine();
 
         System.out.println(d1.username + " " + d1.password + " " + d1.email);
     }
 
-    public boolean searchuser(String username) {
+    public boolean searchuser(String username) {                                                                        // elenxi an yparxei to username ston pinaka kai yparxei se vgazei sthn arxi toy menu
 
         for (User u : Data.datas) {
             if (u.username.equals(username)) {
                 System.out.println("username arleady exist");
                 return true;
-                                                                                                                         // elenxi an yparxei to username ston pinaka kai yparxei se vgazei sthn arxi toy menu
             }
+
+
         }
         return false;
     }
 }
-
--- src/Menus.java
+++ src/Menus.java
@@ -3,34 +3,35 @@ import java.util.Scanner;
 public class Menus {
 
     public void menu() {
-        while (true) {
-            System.out.println("pakalw patiste 1 gia eisodo");
-            System.out.println("pakalw patiste 2 gia dhmioyrgia xrhsth");
-            System.out.println("pakalw patiste 3  gia oles tis egrafes");
-            System.out.println("pakalw patiste 4 gia exodo");
-            Scanner x = new Scanner(System.in);
-            int choice = x.nextInt();
 
-            if (choice == 1) {
-                Login log = new Login();
-                log.userlog();
-            }
-            if (choice == 2) {
-                Create create = new Create();
-                create.create();
-            }
-            if (choice == 3) {
-                for (User a : Data.datas) {
-                    System.out.println(a.username);
-                    System.out.println(a.password);
-                    System.out.println(a.email);
+            while (true) {
+                System.out.println("pakalw patiste 1 gia eisodo");
+                System.out.println("pakalw patiste 2 gia dhmioyrgia xrhsth");
+                System.out.println("pakalw patiste 3  gia oles tis egrafes");
+                System.out.println("pakalw patiste 4 gia exodo");
+                Scanner x = new Scanner(System.in);
+                int choice = x.nextInt();
+
+                if (choice == 1) {
+                    Login log = new Login();
+                    log.userlog();
                 }
+                if (choice == 2) {
+                    Create create = new Create();
+                    create.create();
                 }
-            if (choice == 4) {
-                break;
+                if (choice == 3) {
+                    for (User a : Data.datas) {
+                        System.out.println(a.username);
+                        System.out.println(a.password);
+                        System.out.println(a.email);
+                    }
+                }
+                if (choice == 4) {
+                    break;
+                }
+                return;
             }
-            return;
-    }
 
     }
 
