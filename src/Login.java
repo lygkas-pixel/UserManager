221bc37d365ef1cc8d2da5767bc405638b716005
lygkas-pixel <lygkas.kek@gmail.com>
2026-06-01 23:37:07 +0300

    update

--- src/Login.java
+++ src/Login.java
@@ -7,10 +7,15 @@ public class Login {
     {
         System.out.println("onoma xrhsth1");
         Scanner log = new Scanner(System.in);
-        String username = log.nextLine();
+       String username = log.nextLine();
+
+        Data serchlog = new Data();
+        serchlog.searchuser(username);
         System.out.println("password");
         Scanner pass = new Scanner(System.in);
-        String logpass = pass.nextLine();
+       String logpass = pass.nextLine();
+
+
 
         System.out.println("synxaritiria to onoma xrhsth einai "+ username + " kai o kodikos einai " + logpass );
 
